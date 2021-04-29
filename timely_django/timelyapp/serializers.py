59421d6a27be04fac2ef621dc27d066ead8f2c4c
Nick Fournier <nichfournier@gmail.com>
2021-04-29 12:32:11 -0700

    Removed redundant serializers for payables/receivables. Made single serializer for both, adding bill_to/from prefix

--- timely_django/timelyapp/serializers.py
+++ timely_django/timelyapp/serializers.py
@@ -1,15 +1,12 @@
 # serializers.py
-
-
 from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from rest_auth.registration.serializers import RegisterSerializer
 from rest_auth.serializers import LoginSerializer as RestAuthLoginSerializer
+from phonenumber_field import serializerfields
 
 from rest_framework import serializers
 from .models import *
-
-
 
 class CustomLoginSerializer(RestAuthLoginSerializer):
     username = None
@@ -36,15 +33,24 @@ class CustomRegisterSerializer(RegisterSerializer):
         user.save()
         return user
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'email', 'first_name', 'last_name', 'date_joined']
 
+
 class BusinessSerializer(serializers.ModelSerializer):
     class Meta:
         model = Business
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ['owner', 'date_joined']
+
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        # fields = '__all__'
+        exclude = ['business']
 
 class OrderSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
@@ -60,7 +66,8 @@ class OrderSerializer(serializers.ModelSerializer):
     def get_description(self, obj):
         return Inventory.objects.get(id=obj.item.id).description
 
-class InvoiceSerializer(serializers.ModelSerializer):
+
+class RawInvoiceSerializer(serializers.ModelSerializer):
     bill_from = BusinessSerializer(read_only=True)
     bill_to = BusinessSerializer(read_only=True)
     items = OrderSerializer(many=True, read_only=True)
@@ -69,20 +76,37 @@ class InvoiceSerializer(serializers.ModelSerializer):
         model = Invoice
         fields = '__all__'
 
-class PayablesSerializer(serializers.ModelSerializer):
+class InvoiceSerializer(serializers.ModelSerializer):
     invoice_id = serializers.IntegerField(source='id')
     items = OrderSerializer(many=True, read_only=True)
-    address = serializers.SerializerMethodField()
-    business_name = serializers.SerializerMethodField()
+
+    from_business_name = serializers.SerializerMethodField()
+    from_billing_address = serializers.SerializerMethodField()
+    from_email = serializers.SerializerMethodField()
+    from_phone = serializers.SerializerMethodField()
+
+    to_business_name = serializers.SerializerMethodField()
+    to_billing_address = serializers.SerializerMethodField()
+    to_email = serializers.SerializerMethodField()
+    to_phone = serializers.SerializerMethodField()
 
     class Meta:
         model = Invoice
         fields = ('invoice_id',
                   'invoice_name',
-                  'bill_to',
-                  'bill_from',
-                  'business_name',
-                  'address',
+
+                  'bill_from_id',
+                  'from_business_name',
+                  'from_billing_address',
+                  'from_email',
+                  'from_phone',
+
+                  'bill_to_id',
+                  'to_business_name',
+                  'to_billing_address',
+                  'to_email',
+                  'to_phone',
+
                   'date_sent',
                   'date_due',
                   'terms',
@@ -101,38 +125,90 @@ class PayablesSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
-    def get_address(self, obj):
-        return Business.objects.get(id=obj.bill_from.id).address
-
-    def get_business_name(self, obj):
+    def get_from_business_name(self, obj):
         return Business.objects.get(id=obj.bill_from.id).business_name
+    def get_from_billing_address(self, obj):
+        return Business.objects.get(id=obj.bill_from.id).billing_address
+    def get_from_email(self, obj):
+        return Business.objects.get(id=obj.bill_from.id).billing_address
+    def get_from_phone(self, obj):
+        return Business.objects.get(id=obj.bill_from.id).billing_address
 
-class ReceivablesSerializer(serializers.ModelSerializer):
-    invoice_id = serializers.IntegerField(source='id')
-    items = OrderSerializer(many=True, read_only=True)
-    address = serializers.SerializerMethodField()
-    business_name = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Invoice
-        fields = ('invoice_id',
-                  'invoice_name',
-                  'bill_to',
-                  'bill_from',
-                  'business_name',
-                  'address',
-                  'date_sent',
-                  'date_due',
-                  'terms',
-                  'total_price',
-                  'currency',
-                  'is_flagged',
-                  'is_scheduled',
-                  'is_paid',
-                  'items')
-
-    def get_address(self, obj):
-        return Business.objects.get(id=obj.bill_to.id).address
-
-    def get_business_name(self, obj):
+    def get_to_business_name(self, obj):
         return Business.objects.get(id=obj.bill_to.id).business_name
+    def get_to_billing_address(self, obj):
+        return Business.objects.get(id=obj.bill_to.id).billing_address
+    def get_to_email(self, obj):
+        return Business.objects.get(id=obj.bill_to.id).billing_address
+    def get_to_phone(self, obj):
+        return Business.objects.get(id=obj.bill_to.id).billing_address
+
+
+# OLD SERIALIZERS, MARKED FOR DELETION
+## class PayablesSerializer(serializers.ModelSerializer):
+#     invoice_id = serializers.IntegerField(source='id')
+#     items = OrderSerializer(many=True, read_only=True)
+#     billing_address = serializers.SerializerMethodField()
+#     business_name = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = Invoice
+#         fields = ('invoice_id',
+#                   'invoice_name',
+#                   'bill_to',
+#                   'bill_from',
+#                   'business_name',
+#                   'billing_address',
+#                   'date_sent',
+#                   'date_due',
+#                   'terms',
+#                   'total_price',
+#                   'currency',
+#                   'is_flagged',
+#                   'is_scheduled',
+#                   'is_paid',
+#                   'items')
+#
+#     # Update the instance
+#     def update(self, instance, validated_data):
+#         instance.is_flagged = validated_data['is_flagged']
+#         instance.is_scheduled = validated_data['is_scheduled']
+#         instance.is_paid = validated_data['is_paid']
+#         instance.save()
+#         return instance
+#
+#     def get_billing_address(self, obj):
+#         return Business.objects.get(id=obj.bill_from.id).billing_address
+#
+#     def get_business_name(self, obj):
+#         return Business.objects.get(id=obj.bill_from.id).business_name
+#
+# class ReceivablesSerializer(serializers.ModelSerializer):
+#     invoice_id = serializers.IntegerField(source='id')
+#     items = OrderSerializer(many=True, read_only=True)
+#     address = serializers.SerializerMethodField()
+#     business_name = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = Invoice
+#         fields = ('invoice_id',
+#                   'invoice_name',
+#                   'bill_to',
+#                   'bill_from',
+#                   'business_name',
+#                   'address',
+#                   'date_sent',
+#                   'date_due',
+#                   'terms',
+#                   'total_price',
+#                   'currency',
+#                   'is_flagged',
+#                   'is_scheduled',
+#                   'is_paid',
+#                   'items')
+#
+#     def get_address(self, obj):
+#         return Business.objects.get(id=obj.bill_to.id).address
+#
+#     def get_business_name(self, obj):
+#         return Business.objects.get(id=obj.bill_to.id).business_name
