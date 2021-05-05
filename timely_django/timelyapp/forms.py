797b72790f9e5b199e5d69e8c249080379673655
Nick Fournier <nichfournier@gmail.com>
2021-05-04 21:29:49 -0700

    Commented out old obsolete stuff interfering with current models.

--- timely_django/timelyapp/forms.py
+++ timely_django/timelyapp/forms.py
@@ -30,36 +30,36 @@ class CreateBusinessForm(ModelForm):
         model = Business
         fields = ['business_name', 'billing_address', 'shipping_address']
 
-class CreateOrderForm(ModelForm):
-    class Meta:
-        model = Order
-        fields = ['item', 'quantity_purchased', 'discount_code']
-
-OrderFormSet = inlineformset_factory(
-    Invoice, Order, form=CreateOrderForm,
-    fields=['item', 'quantity_purchased', 'discount_code'], extra=1, can_delete=True
-    )
-
-
-class CreateInvoiceForm(forms.ModelForm):
-    class Meta:
-        model = Invoice
-        fields = ['bill_to', 'terms']
-
-    def __init__(self, *args, **kwargs):
-        super(CreateInvoiceForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_tag = True
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-md-3 create-label'
-        self.helper.field_class = 'col-md-9'
-        self.helper.layout = Layout(
-            Div(
-                Field('bill_to'),
-                Field('terms'),
-                Fieldset('Add orders',
-                    Formset('item')),
-                HTML("<br>"),
-                ButtonHolder(Submit('submit', 'save')),
-                )
-            )
+# class CreateOrderForm(ModelForm):
+#     class Meta:
+#         model = Order
+#         fields = ['item', 'quantity_purchased', 'discount_code']
+#
+# OrderFormSet = inlineformset_factory(
+#     Invoice, Order, form=CreateOrderForm,
+#     fields=['item', 'quantity_purchased', 'discount_code'], extra=1, can_delete=True
+#     )
+#
+#
+# class CreateInvoiceForm(forms.ModelForm):
+#     class Meta:
+#         model = Invoice
+#         fields = ['bill_to', 'terms']
+#
+#     def __init__(self, *args, **kwargs):
+#         super(CreateInvoiceForm, self).__init__(*args, **kwargs)
+#         self.helper = FormHelper()
+#         self.helper.form_tag = True
+#         self.helper.form_class = 'form-horizontal'
+#         self.helper.label_class = 'col-md-3 create-label'
+#         self.helper.field_class = 'col-md-9'
+#         self.helper.layout = Layout(
+#             Div(
+#                 Field('bill_to'),
+#                 Field('terms'),
+#                 Fieldset('Add orders',
+#                     Formset('item')),
+#                 HTML("<br>"),
+#                 ButtonHolder(Submit('submit', 'save')),
+#                 )
+#             )
