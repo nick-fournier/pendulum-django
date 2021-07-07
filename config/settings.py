9a54d70fd56d48184b738bdceef194c6ae555fe2
nick-fournier <nichfournier@gmail.com>
2021-07-07 09:10:55 -0700

    fixed typo in comment

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# This is new:
+# Load .env file
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
