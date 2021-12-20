e667ba191df40c75015f846d3995a00a9cbba268
nick-fournier <nichfournier@gmail.com>
2021-12-19 17:12:29 -0800

    catch none due date

--- timelyapp/mail.py
+++ timelyapp/mail.py
@@ -69,7 +69,7 @@ def send_notification(**args):
             context['user_name'] = None
 
         # Getting email html context
-        if not invoice.terms in ['COD', 'CIA']:
+        if invoice.terms not in ['COD', 'CIA'] and invoice.date_due is not None:
             due_string = invoice.date_due.strftime("%B %d, %Y")
             context['due_string'] = due_string
             context['due_statement'] = 'Please pay by ' + due_string + '.'
