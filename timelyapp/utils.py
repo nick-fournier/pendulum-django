286209d2ccfcad1733e9f6b754368f527c85eb44
nick-fournier <nichfournier@gmail.com>
2022-02-23 10:14:54 -0800

    extract tax id

--- timelyapp/utils.py
+++ timelyapp/utils.py
@@ -173,9 +173,11 @@ def create_invoice(validated_data, business, bill_to_from):
                 Inventory.objects.create(business=business, **new_item)
             item.pop('is_new')
 
-            # Pop out many-to-many payment field. Need to create order before assigning
+            # Pop out many-to-many payment field. Need to create item order before assigning
             if 'item_tax_rates' in item:
                 tax_rates = item.pop('item_tax_rates')
+                # Check if dict or just tax id
+                tax_rates = [x['id'] if type(x) == dict else x for x in tax_rates]
             else:
                 tax_rates = []
             # Create the order
